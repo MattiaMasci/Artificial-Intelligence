be9a626fd5885de9ff38fd23bb97ea586806c6d6
Mattia Masci <mascimattia@gmail.com>
2024-08-29 16:55:49 +0200

    Del

--- 2°Homework/predictive-minmax-master/engine/view.py
+++
@@ -1,7 +0,0 @@
-from chessboard import display
-class View:
-    def __init__(self, board):
-        self.fen = board.fen()
-        display.start(self.fen)
-    def update_board(self, board):
-        display.update(board.fen())
